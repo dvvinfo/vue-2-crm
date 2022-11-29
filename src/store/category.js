cf3c1721b8ebe5907bc600f139951df5af48b1cb
dvvinfo <dvvinfo@mail.ru>
2022-11-29 18:08:41 +0300

    added adding and editing categories

---
+++ src/store/category.js
@@ -0,0 +1,61 @@
+import firebase from "firebase/app";
+export default {
+  actions: {
+    // создаем категорию
+    async createCategory({ commit, dispatch }, { title, limit }) {
+      try {
+        const uid = await dispatch("getUid");
+        const category = await firebase
+          .database()
+          .ref(`/users/${uid}/categories`)
+          .push({ title, limit });
+
+        return { title, limit, id: category.key };
+      } catch (error) {
+        commit("setError", error);
+        throw error;
+      }
+    },
+
+    // редактирование категорий
+    async fetchCategories({ commit, dispatch }) {
+      try {
+        const uid = await dispatch("getUid");
+        const categories =
+          (
+            await firebase
+              .database()
+              .ref(`/users/${uid}/categories`)
+              .once("value")
+          ).val() || [];
+
+        const cats = [];
+        Object.keys(categories).forEach((key) => {
+          cats.push({
+            title: categories[key].title,
+            limit: categories[key].limit,
+            id: key,
+          });
+        });
+        return cats;
+      } catch (error) {
+        commit("setError", error);
+        throw error;
+      }
+    },
+
+    async updateCategory({ commit, dispatch }, { title, limit, id }) {
+      try {
+        const uid = await dispatch("getUid");
+        await firebase
+          .database()
+          .ref(`/users/${uid}/categories`)
+          .child(id)
+          .update({ title, limit });
+      } catch (error) {
+        commit("setError", error);
+        throw error;
+      }
+    },
+  },
+};
